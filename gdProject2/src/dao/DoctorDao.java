bb195428a1786107a6014d5f75c5142a5b5ddf56
DawonOh <dawonohh@gmail.com>
2021-10-22 00:50:12 +0900

    한글깨짐 테스트

--- gdProject2/src/dao/DoctorDao.java
+++ gdProject2/src/dao/DoctorDao.java
@@ -3,6 +3,6 @@ package dao;
 import model.doctor;
 
 public interface DoctorDao {
-	//의사 로그인
+	//로그인
 	doctor login(String id, String pwd);
 }
--- gdProject2/src/dao/ManagerDao.java
+++ gdProject2/src/dao/ManagerDao.java
@@ -3,6 +3,6 @@ package dao;
 import model.manager;
 
 public interface ManagerDao {
-	//관리자 로그인
+	//로그인
 	manager login(String id, String pwd);
 }
