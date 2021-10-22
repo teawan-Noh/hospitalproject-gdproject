12011e86f3a72327754a1175e343dfd23a8d570c
DESKTOP-QA0M42J\taewan <ntw2100@gmail.com>
2021-10-22 18:40:36 +0900

    새로 만든거

--- gdProject2/src/dao/Test.java
+++ gdProject2/src/dao/Test.java
@@ -1,19 +1,42 @@
 package dao;
 
+import java.sql.SQLException;
+import java.util.HashMap;
 import java.util.List;
 
-import model.Subject;
+import model.Qna;
+
 
 public class Test {
 
-	public static void main(String[] args) {
-		// TODO 자동 생성된 메소드 스텁
-		ReservationDao rdao = new ReservationDaoImpl();
-		List<Subject> subjectList = rdao.selectSubjectAll();
+	public static void main(String[] args) throws SQLException {
+		QnaDao dao = new QnaDaoImpl();
 		
-		for(Subject s : subjectList) {
-			System.out.println(s.getName());
+		
+//		void insert(Qna qna);
+//		Qna qna = new Qna();
+//		qna.setName("memo4");
+//		qna.setAge(20);
+		
+//		dao.insert(qna);
+		
+//		void update(Qna qna);
+//		dao.update(memo);
+		
+//		void delete(int qno);
+//		dao.delete(2);
+//		
+//		List<Qna> selectAll();
+//		List<HashMap> list = dao.selectAll();
+		List<HashMap> list = dao.selectByNickname("환자3");
+		for (HashMap hashMap : list) {
+			System.out.println(hashMap.get("qno"));
 		}
+		
+//		Qna selectByQno(int qno);
+//		Qna qna3 = dao.Qna selectByQno(int qno);
+
+		
 	}
 
 }
