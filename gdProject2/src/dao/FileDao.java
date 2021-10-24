8858ab102b15bfe7160248130ace026ca9606c59
82104 <82104@ohj>
2021-10-24 14:19:12 +0900

    dao public 삭제

--- gdProject2/src/dao/FileDao.java
+++ gdProject2/src/dao/FileDao.java
@@ -4,11 +4,11 @@ import model.Files;
 
 public interface FileDao {
 	//게시글 파일 등록
-	public void insert(Files files);
+	void insert(Files files);
 	
 	//게시글 파일 수정
-	public void update(Files files);
+	void update(Files files);
 	
 	//게시글 파일 삭제
-	public void delete(long ncode);
+	void delete(long ncode);
 }
