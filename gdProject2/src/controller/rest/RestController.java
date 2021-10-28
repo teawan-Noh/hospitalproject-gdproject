6050bdb3174ece936688c4c729dfe2f7983fc94f
sunne090 <seonhui7622@gmail.com>
2021-10-28 16:21:35 +0900

    의사 휴진신청 달력에 이벤트 등록 구현(이벤트 하나만 등록되게 수정 예정)

---
+++ gdProject2/src/controller/rest/RestController.java
@@ -0,0 +1,85 @@
+package controller.rest;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import dao.rest.RestDao;
+import dao.rest.RestDaoImpl;
+import model.ask.Rest;
+
+@WebServlet(name = "RestController", urlPatterns={"/rest","/rest_input", "/schedule_check"})
+public class RestController  extends HttpServlet{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 247398011074809924L;
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		req.setCharacterEncoding("UTF-8");
+		process(req, resp);
+	}
+	
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		req.setCharacterEncoding("UTF-8");
+		process(req, resp);
+	}
+	
+	private void process(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		String uri = req.getRequestURI();
+		int lastIndex = uri.lastIndexOf("/");
+		String action = uri.substring(lastIndex + 1);
+	
+		
+		if(action.equals("rest")) {
+			HttpSession session = req.getSession();
+			int dcode = (int)session.getAttribute("dcode");
+			RestDao dao = new RestDaoImpl();
+			List<Rest> waitList = dao.selectRestBydcode("´ë±â", dcode);
+			req.setAttribute("waitList", waitList);
+			List<Rest> restList = dao.selectRestBydcode("½ÂÀÎ", dcode);
+			req.setAttribute("restList", restList);
+		} else if(action.equals("rest_input")) {
+			String date = req.getParameter("date");
+			System.out.println(date);
+			String reason = req.getParameter("reason");
+			System.out.println(reason);
+			HttpSession session = req.getSession();
+			int dcode = (int)session.getAttribute("dcode");
+			String day = req.getParameter("day");
+			Rest rest = new Rest(dcode, reason, date, day);
+			RestDao dao = new RestDaoImpl();
+			dao.insert(rest);
+		} else if(action.equals("schedule_check")) {
+			HttpSession session = req.getSession();
+			int dcode = (int)session.getAttribute("dcode");
+			RestDao dao = new RestDaoImpl();
+			List<Rest> waitList = dao.selectRestBydcode("´ë±â", dcode);
+			req.setAttribute("waitList", waitList);
+			List<Rest> restList = dao.selectRestBydcode("½ÂÀÎ", dcode);
+			req.setAttribute("restList", restList);
+		}
+		
+		String dispatcherUrl = null;
+		if(action.equals("rest")) {
+			dispatcherUrl = "jsp/doctor/doctorRest.jsp";
+		} else if(action.equals("rest_input")) {
+			dispatcherUrl = "rest";
+		} else if(action.equals("schedule_check")) {
+			dispatcherUrl = "jsp/doctor/doctorSchedule.jsp";
+		}
+		
+		RequestDispatcher dispatcher = req.getRequestDispatcher(dispatcherUrl);
+		dispatcher.forward(req, resp);
+	}
+}
--- gdProject2/src/model/ask/Rest.java
+++ gdProject2/src/model/ask/Rest.java
@@ -2,42 +2,83 @@ package model.ask;
 
 public class Rest {
 	private int rcode;
-	private int acode;
+	private int dcode;
+	private String requestdate;
+	private String approved;
+	private String reason;
 	private String restdate;
 	private String day;
 	
 	public Rest() {}
-	
-	public Rest(int rcode, int acode, String restdate, String day) {
+
+	public Rest(int dcode, String reason, String restdate, String day) {
 		super();
-		this.rcode = rcode;
-		this.acode = acode;
+		this.dcode = dcode;
+		this.reason = reason;
 		this.restdate = restdate;
 		this.day = day;
 	}
+
 	public int getRcode() {
 		return rcode;
 	}
+
 	public void setRcode(int rcode) {
 		this.rcode = rcode;
 	}
-	public int getAcode() {
-		return acode;
+
+	public int getDcode() {
+		return dcode;
 	}
-	public void setAcode(int acode) {
-		this.acode = acode;
+
+	public void setDcode(int dcode) {
+		this.dcode = dcode;
 	}
+
+	public String getRequestdate() {
+		return requestdate;
+	}
+
+	public void setRequestdate(String requestdate) {
+		this.requestdate = requestdate;
+	}
+
+	public String getApproved() {
+		return approved;
+	}
+
+	public void setApproved(String approved) {
+		this.approved = approved;
+	}
+
+	public String getReason() {
+		return reason;
+	}
+
+	public void setReason(String reason) {
+		this.reason = reason;
+	}
+
 	public String getRestdate() {
 		return restdate;
 	}
+
 	public void setRestdate(String restdate) {
 		this.restdate = restdate;
 	}
+
 	public String getDay() {
 		return day;
 	}
+
 	public void setDay(String day) {
 		this.day = day;
+	}
+
+	@Override
+	public String toString() {
+		return "Rest [rcode=" + rcode + ", dcode=" + dcode + ", requestdate=" + requestdate + ", approved=" + approved
+				+ ", reason=" + reason + ", restdate=" + restdate + ", day=" + day + "]";
 	}
 	
 	
